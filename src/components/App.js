aa61f98aae8e398feccb03f054348581227a9c5f
Adonis K <aklp08@gmail.com>
2016-10-29 14:35:57 +0300

    added the routing system

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,41 @@
-import scene from '../scenes/sample'
+import { Navigator } from 'react-native'
+import React from 'react'
 
-export default scene
+import rootScene from '../scenes/root'
+import sponsorsScene from '../scenes/sponsors'
+
+const scenes = [
+  {
+    title: 'Root',
+    index: 0,
+  },
+  {
+    title: 'Sponsors',
+    index: 1,
+  },
+]
+
+const renderer = (route, navigator) => {
+  if (!route) {
+    return <sponsorsScene navigator={navigator} />
+  }
+
+  switch (route.id) {
+    case 'Sample':
+      return rootScene
+    case 'Sponsors':
+      return sponsorsScene
+    default:
+      return rootScene
+  }
+}
+
+const App = () => (
+  <Navigator
+    initialRoute={scenes[0]}
+    initialRouteStack={scenes}
+    renderScene={(route, nav) => renderer(route, nav)}
+  />
+)
+
+export default App
